3846940cf16623ce8340de43b686cc6a502cb15f
Joshua <joshua.christiansen@codercamps.com>
2016-09-27 10:57:04 -0700

    lesson complete

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello, world!");
+function doSomething() {
+    var something = "Walk Dog";
+    console.log(something);
+    function doAnotherThing() {
+        var something = "Wash Car";
+        console.log(something);
+    }
+}
+function doSomethingElse() {
+    var something = "Mow Lawn";
+    console.log(something);
+}
+doSomething();
+doSomethingElse();
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log("hello, world!");
+//function scope
+function doSomething() {
+  let something = "Walk Dog";
+  console.log(something);
+  function doAnotherThing() {
+    let something = "Wash Car";
+    console.log(something);
+  }
+}
+function doSomethingElse(){
+  let something = "Mow Lawn";
+  console.log(something);
+}
+
+//log "Walk Dog"
+doSomething();
+
+//log "Mow Lawn"
+doSomethingElse();
